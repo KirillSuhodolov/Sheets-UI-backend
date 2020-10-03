fabfc6217dcdc6f4498fb0d11ae783f728a6bcb7
Alex Voitov <aliaksandr.voitov@gicsoft.com>
2020-10-03 12:48:31 +0300

    WIP

--- src/js/auth/google.js
+++ src/js/auth/google.js
@@ -1,5 +1,14 @@
 const {from} = require("rxjs")
+const {google} = require("googleapis")
+
+const client_id = ""
+const client_secret = ""
+const redirect_uris = [""]
 
 module.exports = {
-    getOAuth$: () => from({})
+    getOAuth$: () => {
+        const googleOAuthClient = new google.auth.OAuth2(
+
+        )
+    }
 }
